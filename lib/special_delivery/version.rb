6148fca9a07d006428f1e6a310919191033b3833
Lawson Kurtz <lawson.kurtz@viget.com>
2014-01-07 16:39:34 -0500

    Fix gem load issue

--- lib/special_delivery/version.rb
+++ lib/special_delivery/version.rb
@@ -1,3 +1,3 @@
 module SpecialDelivery
-  VERSION = "0.1.1"
+  VERSION = "0.1.2"
 end
--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -8,7 +8,7 @@ require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups)
-require "special_delivery"
+require "special-delivery"
 
 module Dummy
   class Application < Rails::Application
